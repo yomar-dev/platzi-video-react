e3af5e3fe51609b00f1257261fba74d2dbad8f6b
Yomar Miranda <mirandayomar@gmail.com>
2018-06-08 04:06:01 -0500

    Hello world desde React

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello World!!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app');
+const saludo = <h1>Hello World!!</h1>
+
+ReactDOM.render(saludo, app);
