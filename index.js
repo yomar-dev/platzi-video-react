e633297eeea939159742300cfb16ca5c201ad5ff
Yomar Miranda <mirandayomar@gmail.com>
2018-06-08 04:45:46 -0500

    Otra forma de utilizar las funciones de React

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app');
 const saludo = <h1>Hello World!!</h1>
 
-ReactDOM.render(<Media />, app);
+render(<Media />, app);
