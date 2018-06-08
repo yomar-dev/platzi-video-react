47bebd934ce7f3336218b9fbd6f86a27e132f0e7
Yomar Miranda <mirandayomar@gmail.com>
2018-06-08 04:40:51 -0500

    Creando el primer componente

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app');
 const saludo = <h1>Hello World!!</h1>
 
-ReactDOM.render(saludo, app);
+ReactDOM.render(<Media />, app);
---
+++ src/playlist/components/media.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Media extends React.Component{
+    render(){
+        return(
+            <div>
+                <div>
+                    <img src="https://as01.epimg.net/futbol/imagenes/2017/01/24/internacional/1485279498_238373_1485280097_sumario_normal.jpg" width={260} height={160} />
+                    <h3>El día que el Barca recuperó la sonrisa</h3>
+                    <p>Ronaldinho</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Media;
