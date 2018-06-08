6ab522b562989b9b054af39d05ee4b2eb0fd44ca
Yomar Miranda <mirandayomar@gmail.com>
2018-06-08 06:16:30 -0500

    Pasar propiedades a los componentes

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app');
 
-render(<Media />, app);
+render(<Media title="El día que el Barca recuperó la sonrisa" author="Ronaldinho" image="https://as01.epimg.net/futbol/imagenes/2017/01/24/internacional/1485279498_238373_1485280097_sumario_normal.jpg" />, app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,12 +3,14 @@ import './media.css';
 
 class Media extends Component{
     render(){
+        const { title, author, image } = this.props
+
         return(
             <div className="contenedor">
                 <div>
-                    <img src="https://as01.epimg.net/futbol/imagenes/2017/01/24/internacional/1485279498_238373_1485280097_sumario_normal.jpg" width={260} height={160} />
-                    <h3>El día que el Barca recuperó la sonrisa</h3>
-                    <p>Ronaldinho</p>
+                    <img src={image} width={260} height={160} />
+                    <h3>{title}</h3>
+                    <p>{author}</p>
                 </div>
             </div>
         )
