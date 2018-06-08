b7b10e1e415c1637600f8a5b9138efb5ced11f7d
Yomar Miranda <mirandayomar@gmail.com>
2018-06-08 05:14:05 -0500

    Dando estilos a los componentes

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import { render } from 'react-dom';
 import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app');
-const saludo = <h1>Hello World!!</h1>
 
 render(<Media />, app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 
 class Media extends Component{
     render(){
+        const styles = {
+            container: {
+                fontSise: 14,
+                background: '#eee',
+                border: '1px solid #bbb',
+                boxSizing: 'border-box',
+                color: '#222',
+                fontFamily: 'sans-serif',
+                padding: '1em',
+                width: '260px'  
+            },
+            image: {
+                width: '100%'
+            }
+        }
         return(
-            <div>
+            <div style={styles.container}>
                 <div>
-                    <img src="https://as01.epimg.net/futbol/imagenes/2017/01/24/internacional/1485279498_238373_1485280097_sumario_normal.jpg" width={260} height={160} />
+                    <img style={styles.image} src="https://as01.epimg.net/futbol/imagenes/2017/01/24/internacional/1485279498_238373_1485280097_sumario_normal.jpg" width={260} height={160} />
                     <h3>El día que el Barca recuperó la sonrisa</h3>
                     <p>Ronaldinho</p>
                 </div>
