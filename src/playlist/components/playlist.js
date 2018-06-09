5120af05c2a46a13816083a2660b2268fb14d191
Yomar Miranda <mirandayomar@gmail.com>
2018-06-09 05:10:02 -0500

    Componente funcional.

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Media from './media';
 import './playlist.css';
 
-class Playlist extends Component{
-    render(){
-        const { data } = this.props
-        const playlist = data.categories[0].playlist
-        console.log(data);
+function Playlist(props){
+    const { data } = props
+    const playlist = data.categories[0].playlist
+    console.log(data);
 
-        return (
-            <div className="Playlist">
-                {
-                    playlist.map((item) => {
-                        return <Media {...item} key={item.id} />
-                    })
-                }
-            </div>
-        )
-    }
+    return (
+        <div className="Playlist">
+            {
+                playlist.map((item) => {
+                    return <Media {...item} key={item.id} />
+                })
+            }
+        </div>
+    )
 }
 
 export default Playlist;
