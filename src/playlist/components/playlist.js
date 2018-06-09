310b2fe0a8c4682ee154f740ef819f44d98c51cb
Yomar Miranda <mirandayomar@gmail.com>
2018-06-09 15:35:49 -0500

    Estructura del proyecto con Containers y UIComponents - Parte 2

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import Media from './media';
 import './playlist.css';
-import Play from '../../icons/components/play'
 
 function Playlist(props){
     const { data } = props
-    const playlist = data.categories[0].playlist
     console.log(data);
 
     return (
         <div className="Playlist">
-            <Play size={50} color="teal" />
             {
-                playlist.map((item) => {
+                props.playlist.map((item) => {
                     return <Media {...item} key={item.id} />
                 })
             }
