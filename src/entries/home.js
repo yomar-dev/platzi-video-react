05fcc8fdf74935b4d634c64dc01195c8e0babdc9
Yomar Miranda <mirandayomar@gmail.com>
2018-06-09 07:01:34 -0500

    Estructura del proyecto con Containers y UIComponents.

---
+++ src/entries/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Home from '../pages/containers/home';
+//import Playlist from './src/playlist/components/playlist';
+//import data from './src/api.json';
+
+const app = document.getElementById('app');
+
+render(<Home />, app);
