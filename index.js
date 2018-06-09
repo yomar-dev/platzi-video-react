56e8b191bfacee2c0e8df61d5a7cf0fca4e49be9
Yomar Miranda <mirandayomar@gmail.com>
2018-06-09 00:41:37 -0500

    Agregando he importando nuestra API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Media from './src/playlist/components/media';
+import data from './src/api.json';
 
 const app = document.getElementById('app');
 
