de629f8dc64d93e0bfaa047e774f24c0bac34caf
Yomar Miranda <mirandayomar@gmail.com>
2018-06-15 05:55:24 -0500

    Botones de Play y Pausa

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component{
 	render(){
 		return(
 			<VideoPlayerLayout>
 				<Title title="Título de nuestro video" />
+				<PlayPause />
 				<Video
 					autoplay={true}
 					src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
