68d05c5035d7d1b3c1c82d89ba357890dbd8ecd4
Yomar Miranda <mirandayomar@gmail.com>
2018-06-15 04:48:23 -0500

    Iniciando el reproductor de video.

---
+++ src/player/components/video-player-layout.js
@@ -0,0 +1,10 @@
+import React, { component } from 'react';
+import './video-player-layout.css';
+
+const VideoPlayerLayout = (props) => (
+	<div className="VideoPlayer">
+		{props.children}
+	</div>
+)
+
+export default VideoPlayerLayout;
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,9 +1,15 @@
-import React, { Component } 'react';
+import React, { Component } from 'react';
+import VideoPlayerLayout from '../components/video-player-layout';
 
 class VideoPlayer extends Component{
 	render(){
 		return(
 			<VideoPlayerLayout>
+				<video 
+					controls
+					autoPlay
+					src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+				/>
 			</VideoPlayerLayout>
 		)
 	}
