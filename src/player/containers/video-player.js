423827c614718b914a37bbf272c6771e69c69326
Yomar Miranda <mirandayomar@gmail.com>
2018-06-15 03:25:33 -0500

    Componente Base VideoPlayer.

---
+++ src/player/containers/video-player.js
@@ -0,0 +1,12 @@
+import React, { Component } 'react';
+
+class VideoPlayer extends Component{
+	render(){
+		return(
+			<VideoPlayerLayout>
+			</VideoPlayerLayout>
+		)
+	}
+}
+
+export default VideoPlayer;
