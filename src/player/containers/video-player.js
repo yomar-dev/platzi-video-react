c5468622af8528b3f92de0f8ce3cae4175d32ced
Yomar Miranda <mirandayomar@gmail.com>
2018-06-15 05:25:43 -0500

    Componente de título del video.

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
+import Title from '../components/title';
 
 class VideoPlayer extends Component{
 	render(){
 		return(
 			<VideoPlayerLayout>
+				<Title title="Título de nuestro video" />
 				<Video
 					autoplay={true}
 					src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
