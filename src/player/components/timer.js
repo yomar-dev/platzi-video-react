b0e672d2a2f84be69503cb8d1c7f5ad373f5ed47
Yomar Miranda <mirandayomar@gmail.com>
2018-06-16 01:15:59 -0500

    Mostrando el tiempo total y transcurrido en minutos y segundos.

--- src/player/components/timer.js
+++ src/player/components/timer.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './timer.css';
 
+function formattedTime(secs){
+	const minutes = parseInt(secs / 60, 10);
+	const seconds = parseInt(secs % 60, 10);
+
+	return `${minutes}:${seconds}`;
+}
 
 function Timer(props){
 	return(
 		<div className="Timer">
 			<p>
-				<span>{props.currentTime} / {props.duration}</span>
+				<span>{formattedTime(props.currentTime)} / {formattedTime(props.duration)}</span>
 			</p>
 		</div>
 	)
